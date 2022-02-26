d5bc276846d045eadc19dc042da8a33e6a5f3c76
Alex <forcablaugrana@gmail.com>
2022-02-26 11:54:44 +0300

    3rd day done

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,23 +1,79 @@
 const getGoods = () => {
     const links = document.querySelectorAll('.navigation-link');
 
-    const getData = () => {
-        fetch('/db/db.json')
+    const renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list');
+
+        goodsContainer.innerHTML = "";
+
+        goods.forEach(good => {
+            const goodBlock = document.createElement('div');
+
+            goodBlock.classList.add('col-lg-3');
+            goodBlock.classList.add('col-sm-6');
+
+            goodBlock.innerHTML = `
+                <div class="goods-card">
+                    <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+                    <img src="db/${good.img}" alt="${good.name}" class="goods-image">
+                    <h3 class="goods-title">${good.name}</h3>
+                    <p class="goods-description">${good.description}</p>  
+                    <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                        <span class="button-price">$${good.price}</span>
+                    </button>
+                </div>
+            `;
+            goodsContainer.append(goodBlock);
+            console.log(good);
+        });
+    };
+
+    const getData = (value, category) => {
+        fetch('/db/db.json')  // https://wildberries-intensive-4c8d1-default-rtdb.europe-west1.firebasedatabase.app/db.json  (альтернатива на FireBase)
             .then((res) => res.json())
-            .then((data) => console.log(data));
+            .then((data) => {
+                const array = category ? data.filter((item) => item[category] === value) : data ;
+
+                /* if (category) {
+                    console.log('yes');
+                } else {
+                    console.log('no');
+                }
+ */
+                // category ? console.log('yes') : console.log('no');
+
+                localStorage.setItem('goods', JSON.stringify(array));
+
+                if (window.location.pathname !== '/goods.html') {
+                    window.location.href = '/goods.html';
+                } else {
+                    renderGoods(array);
+                }
+            }
+            );
     };
 
     links.forEach((link) => {
         link.addEventListener('click', (event) => {
            event.preventDefault();
-           getData();
+           const linkValue = link.textContent;
+           const category = link.dataset.field;
+           
+           getData(linkValue, category);
         });
     });
+
+    if (localStorage.getItem('goods') && window.location.pathname === '/goods.html') {
+        renderGoods(JSON.parse(localStorage.getItem('goods')));
+    }
 
     /* localStorage.setItem('goods', JSON.stringify([1, 2, 3, 4, 5]));
 
     const goods = JSON.parse(localStorage.getItem('goods'));
-    console.log(goods); */
+    console.log(goods); 
+    
+    localStorage.removeItem('goods');
+    console.log(localStorage); */
 } 
 
 getGoods();
--- js/search.js
+++ js/search.js
@@ -2,14 +2,74 @@ const search = function() {
     const input = document.querySelector('.search-block > input');
     const searchBtn = document.querySelector('.search-block > button');
     
+    const renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list');
+
+        goodsContainer.innerHTML = "";
+
+        goods.forEach(good => {
+            const goodBlock = document.createElement('div');
+
+            goodBlock.classList.add('col-lg-3');
+            goodBlock.classList.add('col-sm-6');
+
+            goodBlock.innerHTML = `
+                <div class="goods-card">
+                    <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+                    <img src="db/${good.img}" alt="${good.name}" class="goods-image">
+                    <h3 class="goods-title">${good.name}</h3>
+                    <p class="goods-description">${good.description}</p>  
+                    <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                        <span class="button-price">$${good.price}</span>
+                    </button>
+                </div>
+            `;
+            goodsContainer.append(goodBlock);
+            
+        });
+    };
+
+    const getData = (value) => {
+        fetch('/db/db.json')  // https://wildberries-intensive-4c8d1-default-rtdb.europe-west1.firebasedatabase.app/db.json  (альтернатива на FireBase)
+            .then((res) => res.json())
+            .then((data) => {
+                const array = data.filter(good => good.name.toLowerCase().includes(value.toLowerCase()));
+                /* if (category) {
+                    console.log('yes');
+                } else {
+                    console.log('no');
+                }
+ */
+                // category ? console.log('yes') : console.log('no');
+
+                localStorage.setItem('goods', JSON.stringify(array));
+
+                if (window.location.pathname !== '/goods.html') {
+                    window.location.href = '/goods.html';
+                } else {
+                    renderGoods(array);
+                }
+            }
+            );
+    };
+
+    searchBtn.addEventListener('click', () => {
+        getData(input.value);
+    });
 
     /* input.addEventListener('input', (event) => {
         console.log(event.target.value);
     }); */
 
+    /* try {
     searchBtn.addEventListener('click', () => {
         console.log(input.value);
     });
-}
+    } catch(e) {
+        console.error(e.message);
+    } */
+};
+
+
 
 search();
